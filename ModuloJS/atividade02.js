f64d825d389619e9c8c95f0bc2f07cf1298f646c
Pessoa <ferrnandopssoa022@gmail.com>
2024-09-11 21:29:48 -0300

    Add files via upload
    eu adicionei um codigo quesupostamente calcula e manda a resposta para o console do HTML

---
+++ ModuloJS/atividade02.js
@@ -0,0 +1,20 @@
+let n1 = Number(prompt("Digite o primeiro número"));
+let n2 = Number(prompt("Digite o segundo número"));
+
+let soma = n1 + n2;
+let sub =  n1 - n2;
+let mult = n1 * n2;
+let divisao = n1 / n2;
+let modulo = n1 % n2;
+let potencia = n1 ** n2;
+
+console.log(n1 + n2);
+console.log(soma);
+console.log(sub);
+console.log(mult);
+console.log(divisao);
+console.log(modulo);
+console.log(potencia);
+console.log(n1**0,5);
+console.log(n2**0,5);
+
